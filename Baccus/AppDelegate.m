1cd4be19644b4a33421323be23403a8e21ff0ca8
Bhavish Chandnani <bhavishchandnani@bvc.local>
2016-02-29 20:40:45 +0100

    First commit, project structure and AppDelegate implemented

--- Baccus/AppDelegate.m
+++ Baccus/AppDelegate.m
@@ -7,6 +7,10 @@
 //
 
 #import "AppDelegate.h"
+#import "BVCWineryModel.h"
+#import "BVCWineryTableViewController.h"
+#import "BVCWebViewController.h"
+#import "BVCWineViewController.h"
 
 @interface AppDelegate ()
 
@@ -15,10 +19,55 @@
 @implementation AppDelegate
 
 
+- (UIViewController *) rootViewControllerForPadWithModel:(BVCWineryModel *) wineryModel{
+    
+    // Creamos los controladores y los navigation
+    BVCWineryTableViewController *wineryVC = [[BVCWineryTableViewController alloc]initWithModel:wineryModel style:UITableViewStylePlain];
+    UINavigationController *wineryNavVC = [[UINavigationController alloc] initWithRootViewController: wineryVC];
+    BVCWineViewController *wineVC = [[BVCWineViewController alloc] initWithModel:[wineryVC lastSelectedWine]];
+    UINavigationController *wineNavVC = [[UINavigationController alloc] initWithRootViewController:wineVC];
+    
+    //Creamos el combinador: SplitView
+    UISplitViewController *splitVC = [[UISplitViewController alloc]init];
+    splitVC.viewControllers = @[wineryNavVC, wineNavVC];
+    
+    //Asignamos delegados
+    splitVC.delegate=wineVC;
+    wineryVC.delegate =wineVC;
+    
+    //Devolvemos el VC que se le asignar controlador raíz
+    return splitVC;
+}
+
+- (UIViewController *) rootViewControllerForPhoneWithModel:(BVCWineryModel *) wineryModel{
+    
+    // Creamos los controladores y los navigation
+    BVCWineryTableViewController *wineryVC = [[BVCWineryTableViewController alloc]initWithModel:wineryModel style:UITableViewStylePlain];
+    UINavigationController *wineryNavVC = [[UINavigationController alloc] initWithRootViewController: wineryVC];
+    wineryVC.delegate = wineryVC;
+    
+    return wineryNavVC;
+    
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-	// Override point for customization after application launch.
-	self.window.backgroundColor = [UIColor whiteColor];
+	
+	//Creamos el modelo
+	BVCWineryModel *winery = [[BVCWineryModel alloc] init];
+    
+    //Configuración de controladores, combinadores y delegados en base al tipo de dispositivo
+    UIViewController *rootVC =nil;
+    if (!IS_IPHONE){
+        rootVC = [self rootViewControllerForPadWithModel:winery];
+    }else{
+        rootVC = [self rootViewControllerForPhoneWithModel:winery];
+    }
+    
+    self.window.rootViewController=rootVC;
+	
+	//self.window.backgroundColor = [UIColor whiteColor];
+    [self customizeAppearance];
 	[self.window makeKeyAndVisible];
 	return YES;
 }
@@ -44,5 +93,26 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
 	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+-(void) customizeAppearance{
+    
+    UIColor *wineColor = [UIColor colorWithRed:120.0/255.0
+                                green:10.0/255.0
+                                 blue:10.0/255.0
+                                alpha:1];
+    UIColor *lightYellowColor= [UIColor colorWithRed:255.0/255.0
+                                      green:250.0/255.0
+                                       blue:160.0/255.0
+                                      alpha:1];
+    
+    NSDictionary *attributes = [NSDictionary dictionaryWithObjectsAndKeys: [UIFont fontWithName:@"Cochin-Bold" size:20], NSFontAttributeName,lightYellowColor, NSForegroundColorAttributeName, nil];
+    
+    [[UINavigationBar appearance] setTitleTextAttributes:attributes];
+    [[UINavigationBar appearance] setBarTintColor: wineColor];
+    
+    [[UIBarButtonItem appearance] setTintColor:lightYellowColor];
+    
+}
+
 
 @end
